ccbdcb622eb8d3f847727ec721123048e9e92f4d
MyAngelShibuyaKanon <99974252+MyAngelShibuyaKanon@users.noreply.github.com>
2024-11-30 22:28:17 +0800

    Test Discord Api

--- script.js
+++ script.js
@@ -5,3 +5,38 @@ hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offHamMenu.classList.toggle('active');
 });
+
+let form = document.getElementById("contactForm");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let fullname = document.getElementById("fullName");
+    let email = document.getElementById("email");
+    let company = document.getElementById("company");
+    let message = document.getElementById("message");
+
+    console.log(
+        `This form has a name of ${fullname.value}, 
+        an email of ${email.value}, a company name of ${company.value}, and a message of ${message.value}`
+      );
+
+    fetch("https://f565-124-217-83-118.ngrok-free.app/sendFormDiscord/", {
+        method: "POST",
+        body: JSON.stringify({
+          fullname: fullname.value,
+          email: email.value,
+          company: company.value,
+          message: message.value
+        }),
+
+        headers: {
+          "content-type": "application/json; charset=UTF-8"
+        }
+      })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+});
+
+
+  
